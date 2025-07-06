91c1130d4d9047b8acf50c90c2da57eb0039bf8c
Pablo S <cineschonffeldt@gmail.com>
2025-07-06 09:18:05 -0500

    Control form and submit button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
+
+type value = {
+  value: string;
+  // searchText: string;
+  // setSearchText: string;
+};
+
 export default function SearchBar() {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <div className="search-bar-container">
-      <form className="search-bar">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="search-bar"
+      >
         <input
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+            console.log(searchText);
+          }}
           className="search-bar-input"
           type="text"
           placeholder=" Busca museos por su nombre"
         ></input>
-        <button className="search-bar-button">Buscar</button>
+        <button
+          onClick={() => alert(`A search has been made: ${searchText}`)}
+          type="button"
+          className="search-bar-button"
+        >
+          Buscar
+        </button>
       </form>
     </div>
   );
